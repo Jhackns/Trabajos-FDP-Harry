0d6ad6adf7c70eb27e7af654b0b621cc2c306086
Jhackns <jhackmaniaz64@gmail.com>
2022-05-12 17:57:06 +0000

    EstRepetitivas

--- EstRepetitivas/EjerciciosR.java
+++ EstRepetitivas/EjerciciosR.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.jar.Manifest;
 
 /**
@@ -22,13 +23,26 @@ public class EjerciciosR {
     public static void ImprimirForN1_20() {
         for (int numInit = 1; numInit <= 20; numInit++) {
             System.out.println(numInit);
+            
+        }
+        
+    }
+    public static void nombrevertical() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
+        String nombre=lt.next();
+        for (int i = nombre.length()-1; i >=0; i--) {
+            System.out.println(nombre.charAt(i));
         }
     }
 
     public static void main(String[] args) {
-       System.out.println("Imprime while:"); imprimirWhileN1_20();
+       /*System.out.println("Imprime while:"); imprimirWhileN1_20();
        System.out.println("Imprime Do while:"); imprimirDowhilen1_20();
-       System.out.println("Imprime for:"); ImprimirForN1_20();
+       System.out.println("Imprime for:"); ImprimirForN1_20();*/
+
+       nombrevertical();
+       
     }
         
     }
