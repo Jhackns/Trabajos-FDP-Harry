1498399b19d6a243dc832f907b4ead1b3d72f8af
Jhackns <jhackmaniaz64@gmail.com>
2022-05-12 17:10:24 +0000

    EstRepetitivas

---
+++ EstRepetitivas/EjerciciosR.java
@@ -0,0 +1,35 @@
+import java.util.jar.Manifest;
+
+/**
+ * EjerciciosR
+ */
+public class EjerciciosR {
+
+    public static void imprimirWhileN1_20() {
+        int numInit=20;
+        while (numInit<=20) {
+            System.out.println( numInit);
+            numInit=numInit+1;
+        }
+    }
+    public static void imprimirDowhilen1_20() {
+        int numInit=20;
+        do {
+            System.out.println(numInit);
+            numInit++;
+        } while (numInit<=20);
+    }
+    public static void ImprimirForN1_20() {
+        for (int numInit = 1; numInit <= 20; numInit++) {
+            System.out.println(numInit);
+        }
+    }
+
+    public static void main(String[] args) {
+       System.out.println("Imprime while:"); imprimirWhileN1_20();
+       System.out.println("Imprime Do while:"); imprimirDowhilen1_20();
+       System.out.println("Imprime for:"); ImprimirForN1_20();
+    }
+        
+    }
+
